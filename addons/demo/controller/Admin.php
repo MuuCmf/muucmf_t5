bdbe7b88291f3eba9a5ab4376bb4d7695e57e8ce
dameng <59262424@qq.com>
2018-09-28 08:52:01 +0800

    修复部分大小写错误

--- addons/demo/controller/Admin.php
+++ addons/demo/controller/Admin.php
@@ -1,16 +1,28 @@
 <?php
 namespace addons\demo\controller;
 
+use muucmf\addons\Admin as AddonsAdmin;
 
+/**
+ *插件管理类 
+**/
 
-class Admin extends \muucmf\addons\Admin
+class Admin extends AddonsAdmin
 {
-
 
 	public function index(){
 
 		
 		$this->setTitle('插件管理后台');
+		return $this->fetch();
+	}
+
+	public function test(){
+
+		
+		$this->setTitle('插件管理后台');
+		$test = 'This is a TEST';
+		$this->assign('test',$test);
 		return $this->fetch();
 	}
 
