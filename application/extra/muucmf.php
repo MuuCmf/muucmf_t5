b6d55f8473c7b2ca1fbd1d9e66c9b1b84793c5e2
dameng <59262424@qq.com>
2018-11-08 19:28:08 +0800

    更新小版本号

--- application/extra/muucmf.php
+++ application/extra/muucmf.php
@@ -2,7 +2,7 @@
 //MuuCmf系统配置项 请勿修改
 return [
     //版本号
-    'version'             => '0.0.30.20181020_alpha',
+    'version'             => '0.0.40.20181109_alpha',
     //云接口地址
     'api_url'             => 'http://www.muucmf.cn', 
 ];
