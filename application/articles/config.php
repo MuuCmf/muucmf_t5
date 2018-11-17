397c7f68866bed7ebf65695554169f272d407218
dameng <59262424@qq.com>
2018-11-17 14:43:36 +0800

    遗漏部分补全

--- application/articles/config.php
+++ application/articles/config.php
@@ -5,7 +5,7 @@ return [
     // 视图输出字符串内容替换,留空则会自动进行计算
     'view_replace_str'       => [
         '__ZUI__'       => 'https://cdn.bootcss.com/zui/1.8.1',
-        '__SWIPER__'    => 'https://cdn.bootcss.com/Swiper/3.4.2'
+        '__SWIPER__'    => 'https://cdn.bootcss.com/Swiper/3.4.2',
         '__COMMON__'    => '/static/common',
         '__LIB__'       => '/static/common/lib',
         '__JS__'    	=> '/static/articles/js',
