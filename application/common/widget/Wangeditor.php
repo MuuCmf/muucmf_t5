b145757c30bd85cf1be934f97f144965da8a79f6
dameng <59262424@qq.com>
2019-01-30 08:54:15 +0800

    wangEdito新增配置参数

--- application/common/widget/Wangeditor.php
+++ application/common/widget/Wangeditor.php
@@ -5,8 +5,18 @@ use think\Controller;
 
 class Wangeditor extends Controller
 {
-
-    public function editor($id = 'myeditor', $name = 'content', $default='', $config='', $style='', $param='', $width='100%')
+    /**
+     * { function_description }
+     *
+     * @param      string  $id       The identifier
+     * @param      string  $name     The name
+     * @param      string  $default  默认文本
+     * @param      string  $config   all全部编辑按钮、，mini部分编辑按钮
+     * @param      string  $style    The style
+     * @param      string  $param    The parameter
+     * @param      string  $width    The width
+     */
+    public function editor($id = 'myeditor', $name = 'content', $default='', $config='all', $style='', $param='', $width='100%')
     {
         if(empty($style)) $style='min-height:300px;';
             
