83224ff9e92f09b55fad73acd4d6a56265f25347
dameng <59262424@qq.com>
2019-04-10 10:50:29 +0800

    修复后台清空缓存报错的问题

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -62,25 +62,6 @@ class Common extends Controller
 
         @mkdir($dirname,0777,true);
 
-        function rmdirr($dirname) {
-            if (!file_exists($dirname)) {
-                return false;
-            }
-            if (is_file($dirname) || is_link($dirname)) {
-                return unlink($dirname);
-            }
-            $dir = dir($dirname);
-            if($dir){
-                while (false !== $entry = $dir->read()) {
-                    if ($entry == '.' || $entry == '..') {
-                        continue;
-                    }
-                    rmdirr($dirname . DIRECTORY_SEPARATOR . $entry);
-                }
-            }
-            $dir->close();
-            return rmdir($dirname);
-        }
     }
 
 }
