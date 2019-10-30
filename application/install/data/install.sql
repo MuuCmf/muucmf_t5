ffdc173cc8e934feb632925d5d0599d1e93b7a04
dameng <59262424@qq.com>
2019-10-30 09:24:31 +0800

    安装模块脚本及文章演示模块脚本错误处理

--- application/install/data/install.sql
+++ application/install/data/install.sql
@@ -143,58 +143,6 @@ CREATE TABLE IF NOT EXISTS `muucmf_announce_arrive` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='公告确认记录' AUTO_INCREMENT=12 ;
 
-DROP TABLE IF EXISTS `muucmf_articles`;
-CREATE TABLE IF NOT EXISTS `muucmf_articles` (
-  `id` int(11) unsigned NOT NULL COMMENT '自增ID',
-  `uid` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL COMMENT '标题',
-  `keywords` varchar(255) NOT NULL COMMENT '关键字，多个用,分割',
-  `description` varchar(200) NOT NULL COMMENT '描述',
-  `category` int(11) NOT NULL COMMENT '分类',
-  `status` tinyint(2) NOT NULL COMMENT '状态',
-  `reason` varchar(100) NOT NULL COMMENT '审核失败原因',
-  `sort` int(5) NOT NULL COMMENT '排序',
-  `position` int(4) NOT NULL COMMENT '定位，展示位',
-  `cover` int(11) NOT NULL COMMENT '封面',
-  `view` int(10) NOT NULL COMMENT '阅读量',
-  `comment` int(10) NOT NULL COMMENT '评论量',
-  `collection` int(10) NOT NULL COMMENT '收藏量',
-  `source` varchar(200) NOT NULL COMMENT '来源url',
-  `create_time` int(11) NOT NULL,
-  `update_time` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章' AUTO_INCREMENT=9;
-
-DROP TABLE IF EXISTS `muucmf_articles_category`;
-CREATE TABLE IF NOT EXISTS `muucmf_articles_category` (
-  `id` int(11) unsigned NOT NULL COMMENT '自增ID',
-  `title` varchar(20) NOT NULL,
-  `pid` int(11) NOT NULL,
-  `can_post` tinyint(4) NOT NULL COMMENT '前台可投稿',
-  `need_audit` tinyint(4) NOT NULL COMMENT '前台投稿是否需要审核',
-  `sort` tinyint(4) unsigned NOT NULL DEFAULT '0',
-  `status` tinyint(4) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='文章分类';
-
-INSERT INTO `muucmf_articles_category` (`id`, `title`, `pid`, `can_post`, `need_audit`, `sort`, `status`) VALUES
-(1, '早期创业', 0, 1, 1, 3, 1),
-(2, '独角兽', 0, 1, 1, 3, 1),
-(3, '投融资', 0, 1, 1, 4, 1),
-(4, '火木动态', 0, 1, 1, 3, 1),
-(5, '锐观察', 0, 1, 1, 4, 1),
-(6, '技术创新', 0, 1, 1, 5, 1),
-(7, 'test', 0, 1, 1, 8, -1),
-(8, 'test2', 0, 1, 1, 2, -1);
-
-DROP TABLE IF EXISTS `muucmf_articles_detail`;
-CREATE TABLE IF NOT EXISTS `muucmf_articles_detail` (
-  `articles_id` int(11) NOT NULL,
-  `content` text NOT NULL COMMENT '内容',
-  `template` varchar(50) NOT NULL COMMENT '模板',
-  PRIMARY KEY (`articles_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章详情';
-
 DROP TABLE IF EXISTS `muucmf_attachment`;
 CREATE TABLE IF NOT EXISTS `muucmf_attachment` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
